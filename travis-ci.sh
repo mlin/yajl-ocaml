91b4b9c2e9e27dcc66f17be922c55ac9ba90c34a
Mike Lin <mlin@mlin.net>
2013-02-10 18:23:53 -0800

    attempt to run tests on Travis CI

---
+++ travis-ci.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+#This script runs the unit tests inside a Travis CI worker. See also .travis.yml
+
+OPAM_VERSION=0.9.1
+
+# install ocaml from apt
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml
+
+# install opam
+curl -L https://github.com/OCamlPro/opam/archive/$(OPAM_VERSION).tar.gz | tar xz
+pushd opam-$(OPAM_VERSION)
+./configure
+make
+sudo make install
+popd
+opam init
+eval `opam config -env`
+
+# install packages from opam
+opam install ocamlfind ounit
+
+# compile & run tests
+make test
